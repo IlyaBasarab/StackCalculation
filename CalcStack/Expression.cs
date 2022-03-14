b36cdacd55f37f638c63461b6d23ef6f7b6a8992
IlyaBasarab <town_prok@mail.ru>
2022-03-14 22:46:52 +0100

    Přidejte soubory projektu.

---
+++ CalcStack/Expression.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcStack
+{
+    internal class Expression
+    {
+       
+        public string operators = "+-*/";
+
+        public string[] Splitter(string dataStr)
+        {
+
+            if (dataStr.Contains("(") && dataStr.Contains(")"))
+            {
+                string[] data = dataStr.Split(new char[] { '(', ')' });
+                return data;
+            }
+            else
+            {
+                string[] data = dataStr.Split(' ');
+                return data;
+            }
+
+        }
+
+        public int GetResult(string dataInStr)
+        {
+            Stack<string>stack= new Stack<string>();
+             string[] data= Splitter(dataInStr);
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                string[] data2= Splitter(data[i]);
+                string operation = null;
+                for (int j = 0; j < data2.Length; j++)
+                {
+
+                    string token = data2[j];
+                    
+
+                    if (operators.IndexOf(token) == -1)
+                    {
+                        stack.Push(token);
+                    }
+                    else
+                    {
+                        operation = token;
+                    }
+
+                    if (stack.Count == 2)
+                    {
+                        int number2 = Convert.ToInt32(stack.Pop());
+                        int number1 = Convert.ToInt32(stack.Pop());
+
+                        int opNumb = operators.IndexOf(operation);
+                        switch (opNumb)
+                        {
+                            case 0:
+                                stack.Push((number1 + number2).ToString());
+                                break;
+                            case 1:
+                                stack.Push((number1 - number2).ToString());
+                                break;
+                            case 2:
+                                stack.Push((number1 * number2).ToString());
+                                break;
+                            case 3:
+                                stack.Push((number1 / number2).ToString());
+                                break;
+                        }
+                    }
+
+                }
+            }
+            int result = Convert.ToInt32( stack.Pop());
+            return result;
+
+
+        }
+
+
+    }
+}
---
+++ CalcStack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] data = { "10","2","+","2","*"};
+            string data2 = "(20 + 30) * 100 + 50";
+
+            Expression expression = new Expression();
+            Console.WriteLine(expression.GetResult(data2));
+
+        }
+    }
+}
